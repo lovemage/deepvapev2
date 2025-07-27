31c8ccbb2e4b15fc8cb90fbbf4c76a72af02ef79
lovemage <lovemage@gmail.com>
2025-07-28 00:53:08 +0800

    修復XLSX庫導入

--- backend/src/routes/admin.js
+++ backend/src/routes/admin.js
@@ -2,6 +2,7 @@ const express = require('express');
 const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
 const { dbAsync } = require('../database/db');
+const XLSX = require('xlsx');
 const router = express.Router();
 const multer = require('multer');
 const fs = require('fs');
